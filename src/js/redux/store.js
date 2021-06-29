d25d4fa35324baf74d0a32bf3cdc86a2c988e5fc
Hein Thant Maung Maung <me@heinthanth.com>
2021-06-29 22:30:26 +0630

    just patching redux enhancer

--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { compose, createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import AllReducers from "./reducers";
@@ -6,10 +6,11 @@ import AllReducers from "./reducers";
 const persistConfig = { key: "hh-root", storage };
 const persistedReducer = persistReducer(persistConfig, AllReducers);
 
-export const store = createStore(
-    persistedReducer,
-    process.env.NODE_ENV === "production" &&
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers =
+    process.env.NODE_ENV === "production"
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+        : compose;
+const storeEnhancers = composeEnhancers();
+
+export const store = createStore(persistedReducer, storeEnhancers);
 export const persistor = persistStore(store);
