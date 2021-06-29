5e811ad8f8554eb46b8b520e1c40681731bfd78a
Hein Thant Maung Maung <me@heinthanth.com>
2021-06-29 11:47:03 +0630

    initial webpage

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { render, hydrate } from "react-dom";
-import HeinHein from "./js/app";
-
-const mountpoint = document.querySelector("#__hh_app_");
-
-if (mountpoint.hasChildNodes()) hydrate(<HeinHein />, mountpoint);
-else render(<HeinHein />, mountpoint);
