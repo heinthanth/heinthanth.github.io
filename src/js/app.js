b710cbfc7805c695bec42ef2613198efc0f2dc31
Hein Thant Maung Maung <me@heinthanth.com>
2021-06-30 14:17:58 +0630

    add GA for some information

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import "../sass/app.sass";
 import loadable from "@loadable/component";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 const HomePage = loadable((props) => import("./pages/home"));
 const NavBar = loadable((props) => import("./components/navbar"));
@@ -14,32 +16,45 @@ const HeinHein = () => {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <App />
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
             </PersistGate>
         </Provider>
     );
 };
 
 const App = () => {
+    const location = useLocation();
     const theme = useSelector((state) => state.theme);
 
+    useEffect(() => {
+        ReactGA.initialize("UA-140479987-2", {
+            debug: process.env.NODE_ENV === "development",
+        });
+    }, []);
+
+    useEffect(() => {
+        const currentPath = location.pathname + location.search;
+        ReactGA.set({ page: currentPath });
+        ReactGA.pageview(currentPath);
+    }, [location]);
+
     return (
-        <BrowserRouter>
-            <div id="hh-root" data-hh-theme={theme}>
-                <NavBar />
-                <Switch>
-                    <Route path="/" exact>
-                        <HomePage />
-                    </Route>
-                    <Route path="/admin">
-                        <AdminPage />
-                    </Route>
-                    <Route>
-                        <Error404 />
-                    </Route>
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <div id="hh-root" data-hh-theme={theme}>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact>
+                    <HomePage />
+                </Route>
+                <Route path="/admin">
+                    <AdminPage />
+                </Route>
+                <Route>
+                    <Error404 />
+                </Route>
+            </Switch>
+        </div>
     );
 };
 
