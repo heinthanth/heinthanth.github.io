a164090c8fd6c3e81f4f247307f3fab04570f59b
Hein Thant Maung Maung <me@heinthanth.com>
2021-06-30 07:35:31 +0630

    disable devtools on production

--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -6,11 +6,10 @@ import AllReducers from "./reducers";
 const persistConfig = { key: "hh-root", storage };
 const persistedReducer = persistReducer(persistConfig, AllReducers);
 
-const composeEnhancers =
+const composedEnhancers =
     process.env.NODE_ENV === "production"
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-        : compose;
-const storeEnhancers = composeEnhancers();
+        ? compose
+        : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(persistedReducer, storeEnhancers);
+export const store = createStore(persistedReducer, composedEnhancers());
 export const persistor = persistStore(store);
