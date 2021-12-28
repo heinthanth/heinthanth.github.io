9adbe46f87ac2950ca267b94a4153750cccce5d2
Hein Thant Maung Maung <me@heinthanth.com>
2021-12-28 20:48:32 +0630

    add redux pages and use .env as URL source

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const siteName = "Hein Thant's Space";
-export const siteURL = "https://heinthant.space";
+export const siteURL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 export const title = "HEIN THANT ( H. T. MAUNG MAUNG )";
 export const titleGen = (t: string) => `${t} - ${title}`;
 // prettier-ignore
